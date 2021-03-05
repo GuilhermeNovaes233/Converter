b1b2a61842a3e0f494261feef314963a00ceb51a
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-03-05 15:00:13 -0300

    Criando api de geração de pdf utilizando Aspose

---
+++ Controllers/GeneratePdfController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Aspose.Pdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Converter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GeneratePdfController : ControllerBase
+    {
+        private static readonly string _dataDir = "..\\..\\..\\Samples";
+
+        [HttpGet]
+        public void GeneratePdf()
+        {
+            // Initialize document object
+            Document document = new Document();
+            // Add page
+            Page page = document.Pages.Add();
+            // Add text to new page
+            page.Paragraphs.Add(new Aspose.Pdf.Text.TextFragment("Hello World!"));
+            // Save updated PDF
+            var outputFileName = System.IO.Path.Combine(_dataDir, "HelloWorld_out.pdf");
+            document.Save(outputFileName);
+        }
+    }
+}
